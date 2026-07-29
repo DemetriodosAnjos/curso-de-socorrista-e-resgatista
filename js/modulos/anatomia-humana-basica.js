c6b7c585bf658253e7d4aeccda9c504991a567b7
Demetrio dos Anjos <ddoanjos@gmail.com>
2026-07-28 22:03:42 -0300

    style: atualiza paleta para azul, ajusta título do header e espaçamentos

--- js/modulos/anatomia-humana-basica.js
+++ js/modulos/anatomia-humana-basica.js
@@ -21,7 +21,6 @@ export const anatomiaHumanaBasica = {
             sistemas do corpo humano para
           </p>
           
-          <br>
           <h1>Sistema Tegumentar</h1>
           
           <p>
@@ -32,9 +31,8 @@ export const anatomiaHumanaBasica = {
 
           <img src="images/anatomia-humana-basica/sistema-tegumentar.png" alt="Sistema Tegumentar e Seus Anexos">
 
-          <br></br>
           <p>
-            <b>Componentes do sistema tegumentar:</b>
+            <h3>Componentes do sistema tegumentar:</h3>
           </p>
           <ul class="lista-sistema-tegumentar">
             <li>Pele;</li>
@@ -46,10 +44,8 @@ export const anatomiaHumanaBasica = {
           
           <img src="images/anatomia-humana-basica/componentes-sistema-tegumentar.png" alt="Componentes do Sistema Tegumentar">
 
-          <br></br>
           <h1>Principais Funções</h1>
 
-          <br></br>
           <ul class="lista-principais-funcoes">
             <li><b>1.Proteção</b> 
                 <p>
@@ -58,34 +54,33 @@ export const anatomiaHumanaBasica = {
                 </p>
             </li>
             
-            <li><b>2.Regulação da temperatura</b> 
+            <li><strong>2.Regulação da temperatura</strong> 
                 <p>
                   Suor ajuda a resfriar o corpo Vasodilatação e vasoconstrição controlam o calor.
                 </p>
             </li>
            
-            <li><b>3.Sensibilidade</b> 
+            <li><strong>3.Sensibilidade</strong> 
                 <p>
                   A pele possui receptores que percebem: Dor, pressão, temperatura e toque.
                 </p>
             </li>
 
-            <li><b>4.Excreção</b> 
+            <li><strong>4.Excreção</strong> 
                 <p>
                   Eliminação de pequenas quantidades de substâncias pelo suor (água, sais minerais).
                 </p>
             </li>
 
-            <li><b>5.Produção de vitamina D</b> 
+            <li><strong>5.Produção de vitamina D</strong> 
                 <p>
                   A pele auxilia na síntese de vitamina D através da luz solar.
                 </p>
             </li>
           </ul>
 
-          <br></br>
           <h1>Importância para o socorrista</h1>
-          <p>
+    
             No atendimento pré-hospitalar, o sistema tegumentar ajuda a identificar:
             <ul class="lista-identificar">
               <li>Feridas e hemorragias</li>
@@ -94,10 +89,9 @@ export const anatomiaHumanaBasica = {
               <li>Choque (pele fria e pálida)</li>
               <li>Infecções e lesões cutâneas</li>
             </ul>
-          </p>
+          
           <img src="images/anatomia-humana-basica/identificar-sintomas.png" alt="O Sistema Tegumentar Ajuda a Identificar">
 
-          <br></br>
           <h1>Sistema Esquelético</h1>
             <p>
               O sistema esquelético é formado pelos ossos, cartilagens, ligamentos e articulações, sendo responsável pela
@@ -105,17 +99,16 @@ export const anatomiaHumanaBasica = {
             </p>
 
             <p>
-              <b>Esqueleto axial</b> (Proteção e sustentação central do corpo.)
+              <strong>Esqueleto axial</strong> (Proteção e sustentação central do corpo.)
               <ul class="Lista-esqueleto-axial">
                 <li>Crânio</li>
                 <li>Coluna vertebral</li>
                 <li>Costelas e esterno</li>
-              </ul>
-           
+              </ul>      
             </p>
             
             <p>
-              <b>Esqueleto apendicular</b> (Locomoção e movimentos.)
+              <strong>Esqueleto apendicular</strong> (Locomoção e movimentos.)
               <ul class="Lista-esqueleto-apendicular">
                 <li>Membros superiores (ombro, braço, antebraço, mão)</li>
                 <li>Membros inferiores (quadril, coxa, perna, pé)</li>
@@ -124,84 +117,258 @@ export const anatomiaHumanaBasica = {
             </p>
             <img src="images/anatomia-humana-basica/sistema-esqueletico.png" alt="Sistema Esquelético">
 
-            <br></br>
-            <h1>Funções Principais</h1>
+          <h1>Funções Principais</h1>
               
-              <ul class="lista-funcoes">
+            <ul class="lista-funcoes">               
+              <li>
+                <strong>Sustentação:</strong>
+                  Mantém a estrutura do corpo e a postura.
+              </li>
+                  
+              <li>
+                <strong>Proteção:</strong>
+                protege órgãos vitais como:
+              </li>
+                  
+              <li>
+                <strong>Crânio:</strong>
+                Protege o cérebro
+              </li>
+
+              <li>
+                <strong>Caixa toráxica:</strong>
+                Protege o coração e os pulmões
+              </li>
+
+              <li>
+                <strong>Coluna vertebral:</strong>
+                Protege a medula espinhal
+              </li>
+
+              <li>
+                <strong>Movimento:</strong>
+                Atua em conjunto com músculos (sistema locomotor).
+              </li>
+
+              <li>
+                <strong>Produção de células sanguíneas:</strong>
+                Na medula óssea (hematopoiese).
+              </li>
+
+              <li>
+                <strong>Armazenamento de minerais:</strong>
+                principalmente cálcio e fósforo.
+              </li>
+            </ul>
+
+            <h1>Classificação dos Ossos</h1>
+              
+              Os ossos do corpo humano são classificados em:
+
+              <ul class="lista-classificacao-ossos">
+                <li>Ossos longos;</li>
+                <li>Ossos curtos;</li>
+                <li>Ossos laminares (planos);</li>
+                <li>Ossos alongados;</li>
+                <li>Ossos pneumáticos;</li>
+                <li>Ossos irregulares;</li>
+                <li>Ossos sesamoides;</li>
+                <li>Ossos suturais.</li>
+              </ul>
+              <img src="images/anatomia-humana-basica/classificacao-ossos.png" alt="Classificação dos Ossos">
+
+            <h1>Crânio e suas Divisões</h1>
+              
+              As divisões do Crânio são classificados em:
+
+              <ul class="lista-classificacao-ossos">
+                <li>Frontal;</li>
+                <li>Nasal;</li>
+                <li>Maxilar;</li>
+                <li>Zigoma;</li>
+                <li>Mandíbula;</li>
+                <li>Parietal;</li>
+                <li>Temporal;</li>
+                <li>Occipital;</li>
+                <li>Processo estilóide temporal;</li>
+              </ul>
                 
-                <li>
-                  <b>Sustentação:</b>
-                    Mantém a estrutura do corpo e a postura.
-                </li>
-                  
-                <li>
-                  <b>Proteção:</b>
-                  protege órgãos vitais como:
-                </li>
-                  
-                <li>
-                  <b>Crânio:</b>
-                  Protege o cérebro
-                </li>
+              <img src="images/anatomia-humana-basica/cranio-divisoes.png" alt="Divisoes do Crânio">
 
-                <li>
-                  <b>Caixa toráxica:</b>
-                  Protege o coração e os pulmões
-                </li>
+            <h1>Importância para o socorrista</h1>
 
-                <li>
-                  <b>Coluna vertebral:</b>
-                  Protege a medula espinhal
-                </li>
-
-                <li>
-                  <b>Movimento:</b>
-                  Atua em conjunto com músculos (sistema locomotor).
-                </li>
-
-                <li>
-                  <b>Produção de células sanguíneas:</b>
-                  Na medula óssea (hematopoiese).
-                </li>
-
-                <li>
-                  <b>Armazenamento de minerais:</b>
-                  principalmente cálcio e fósforo.
-                </li>
+              <ul class="lista-importancia-cranio">
+                <li>Identificar fraturas e luxações;</li>
+                <li>Imobilizar corretamente para evitar agravamento;</li>
+                <li>Reconhecer sinais de trauma: dor intensa;</li>
+                <li>Deformidade;</li>
+                <li>Inchaço;</li>
+                <li>Perda de movimento ;</li>
+                <li>Evitar movimentação desnecessária da vítima.</li>
               </ul>
 
-              <br></br>
-              <h1>Classificação dos Ossos</h1>
-                <p>Os ossos do corpo humano são classificados em:</p>
+            <h1>Sistema Muscular Humano</h1>
+                
+              As principais funções do sistema muscular humano são:
 
-                <ul class="lista-classificacao-ossos">
-                  <li>Ossos longos;</li>
-                  <li>Ossos curtos;</li>
-                  <li>Ossos laminares (planos);</li>
-                  <li>Ossos alongados;</li>
-                  <li>Ossos pneumáticos;</li>
-                  <li>Ossos irregulares;</li>
-                  <li>Ossos sesamoides;</li>
-                  <li>Ossos suturais.</li>
+              <ul class="lista-sistema-muscular-humano">
+                <li>Movimento corporal;</li>
+                <li>Manutenção da postura;</li>
+                <li>Produção de calor.</li>  
+              </ul>
+
+              <p>
+                <strong>OBS:</strong> O corpo humano possui cerca de 600 músculos.
+                </p>
+              
+             <h1>Principais músculos do corpo humano</h1>
+              
+              <ul class="lista-musculo-humano">
+                <li>Esternocleidomastideo;</li>
+                <li>Trapézio;</li>
+                <li>Deltóide;</li>
+                <li>Peitoral maior;</li>
+                <li>Reto abdominal;</li>
+                <li>Oblíquo externo do abdome;</li>
+                <li>Latíssimo do dorso;</li>
+                <li>Tríceps braquial;</li>
+                <li>Bíceps braquial;</li>
+                <li>Flexores de dedos;</li>
+                <li>Extensores de dedos;</li>
+                <li>Glúteo máximo;</li>
+                <li>Sartório;</li>
+                <li>Adutor longo;</li>
+                <li>Reto femoral;</li>
+                <li>Semi membranoso;</li>
+                <li>Bíceps femoral;</li>
+                <li>Gastrocnêmio;</li>
+                <li>Sóleo.</li>  
+              </ul>
+
+              <img src="images/anatomia-humana-basica/principais-musculos.png" alt="Principais Músculos do Corpo Humano">
+
+            <h1>Importância para o socorrista</h1>
+              Principais lesões musculares:
+                <ul class="lista-importancia">
+                  <li>Quedas;</li>
+                  <li>Acidentes;</li>
+                  <li>Torções;</li>
+                  <li>Esmagamentos;</li>
+                  <li>Traumas diversos.</li>
                 </ul>
-                <img src="images/anatomia-humana-basica/classificacao-ossos.png" alt="Classificação dos Ossos">
 
-                <br></br>
-                <h1>Crânio e suas Divisões</h1>
-                  <p>As divisões do Crânio são classificados em:</p>
+            <h1>Sistema Respiratório</h1>
+              <p>
+                O sistema respiratório é responsável pela entrada de oxigênio (O₂) no corpo e pela eliminação de gás carbônico (CO₂), garantindo a respiração celular e a sobrevivência
+              </p>
 
-                <ul class="lista-classificacao-ossos">
-                  <li>Frontal;</li>
-                  <li>Nasal;</li>
-                  <li>Maxilar;</li>
-                  <li>Zigoma;</li>
-                  <li>Mandíbula;</li>
-                  <li>Parietal;</li>
-                  <li>Temporal;</li>
-                  <li>Occipital;</li>
-                  <li>Processo estilóide temporal;</li>
+              <h1>Estrutura do sistema respiratório:</h1>
+                <strong>Vias aereas superiores</strong>
+                <ul class="lista-vias-aereas-superiores">
+                    <li>Nariz e cavidade nasal;</li>
+                    <li>Faringe;</li>
+                    <li>Laringe.</li>
                 </ul>
-                <img src="images/anatomia-humana-basica/cranio-divisoes.png" alt="Divisoes do Crânio">
+                    
+               
+                <strong>Vias aereas inferiores</strong>
+                  <ul class="lista-vias-aereas-inferiores">
+                    <li>Traqueia;</li>
+                    <li>Brônquios;</li>
+                    <li>Bronquíolos;</li>
+                    <li>Alvéolos pulmonares.</li>
+                  </ul>
+                
+                <img src="images/anatomia-humana-basica/sistema-respiratorio.png" alt="Sistema Respiratório">
+                
+              <h1>Principais funções</h1>
+                <ul class="lista-principais-funcoes">
+                    <li>Realizar a troca gasosa (oxigênio entra e CO₂ sai);</li>
+                    <li>Manter a oxigenação do sangue;</li>
+                    <li>Contribuir para o equilíbrio do pH do corpo;</li>
+                    <li>Permitir a fala e o olfato.</li>
+                </ul>
+
+              <h1>Importância para o socorrista</h1>
+                <p>
+                  O sistema respiratório é um dos mais críticos na emergência, pois a falta de oxigênio pode levar à morte em poucos minutos.
+                </p>
+
+                <strong>O Socorrista deve observar:</strong>
+                <ul class="lista-observacoes">
+                    <li>Frequência respiratória (rápida, lenta ou ausente);</li>
+                    <li>Presença de esforço para respirar;</li>
+                    <li>Cianose (lábios/pele arroxeados);</li>
+                    <li>Ruídos respiratórios anormais;</li>
+                    <li>Uso de musculatura acessória.</li>
+                </ul>
+              
+              <h1>Principais emergências respiratórias</h1>
+                <ul class="lista-emergÊncias-respiratorias">
+                    <li>Engasgo (obstrução de vias aéreas);</li>
+                    <li>Crises de asma;</li>
+                    <li>Afogamento;</li>
+                    <li>Pneumotórax (ar fora do pulmão);</li>
+                    <li>Insuficiência respiratória;</li>
+                    <li>Parada respiratória.</li>
+                </ul>
+
+              <h1>Sistema Circulatório</h1>
+                <p>
+                  O sistema circulatório, também chamado de sistema cardiovascular, é responsável por transportar sangue,
+                  oxigênio, nutrientes, hormônios e componentes do sistema.
+                </p>
+
+                <p><strong>Componentes do sistema</strong></p>
+
+                  O Coração é o órgão central que funciona como uma “bomba”, responsável por impulsionar o sangue. Suas principais divisões são:
+                  <ul class="lista-coracao">
+                    <li>Artéria aorta (torácica);</li>
+                    <li>Átrio direito;</li>
+                    <li>Ventrículo direito;</li>
+                    <li>Artéria pulmonar;</li>
+                    <li>Átrio esquerdo;</li>
+                    <li>Válvulas cardíacas;</li>
+                    <li>Ventrículo esquerdo.</li>
+                  </ul>
+
+                  <img src="images/anatomia-humana-basica/sistema-circulatorio.png" alt="Sistema Circulatório">
+              
+              <h1>Vasos sanguíneos</h1>
+                <ul class="lista-vasos-sanguíneos">
+                  <li><strong>Artérias:</strong> levam o sangue do coração para o corpo;</li>
+                  <li><strong>Veias:</strong> trazem o sangue de volta ao coração;</li>
+                  <li><strong>Capilares:</strong> realizam trocas de substâncias com os tecidos;</li>
+                </ul>
+
+              <h1>Sangue</h1>
+                
+                Os principais componentes do sangue são:
+                  <ul class="lista-coracao">
+                    <li>Leucócito Basófilo;</li>
+                    <li>Leucócito Eosinófilo;</li>
+                    <li>Leucócito Monócito;</li>
+                    <li>Glóbulos Vermelhos;</li>
+                    <li>Leucócito Neutrófilo;</li>
+                    <li>Plaquetas;</li>
+                    <li>Plasma;</li>
+                    <li>Linfócilo;</li>
+                    <li>Plasma(60%);</li>
+                    <li>Glóbulos Brancos;</li>
+                    <li>Leucócito Vermelhos.</li>
+                  </ul>
+                <img src="images/anatomia-humana-basica/composicao-sangue.png" alt"Composição do Sangue">
+               
+              <h1>Principais funções</h1>
+                As principais funções do sangue são:
+                  <ul class="lista-funcoes-sangue">
+                    <li>Levar oxigênio (O₂) e nutrientes às células;</li>
+                    <li>Remover gás carbônico (CO₂) e toxinas;</li>
+                    <li>Transportar hormônios;</li>
+                    <li>Manter a temperatura corporal;</li>
+                    <li>Participar da defesa do organismo (células de defesa no sangue).</li>
+                  </ul>
+      
 
     `,
   downloadUrl: "https://drive.google.com/...links-do-drive",
